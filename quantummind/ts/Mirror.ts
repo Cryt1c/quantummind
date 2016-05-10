bb37151d8be4d919de09a8fa91fb2e91c9e16bf1
Dominik Seitz <seitzdo@gmail.com>
2016-05-10 11:20:23 +0200

    added class Mirror

---
+++ quantummind/ts/Mirror.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Dominik on 10.05.2016.
+ */
+
+class Mirror extends GameElement {
+    constructor(xPos:number, yPos:number, public alignment:Alignment) {
+        super(xPos, yPos, 1, 1);
+    }
+}
+
+enum Alignment{
+    TOP_LEFT_TO_BOTTOM_RIGHT,
+    BOTTOM_LEFT_TO_TOP_RIGHT
+}
