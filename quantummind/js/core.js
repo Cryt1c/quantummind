b0590b2125f0ec312b455bd8e5bd90e73f90e620
Dominik Seitz <seitzdo@gmail.com>
2016-05-10 15:27:05 +0200

    remove js directory from vcs

--- quantummind/js/core.js
+++ quantummind/js/core.js
@@ -8,21 +8,30 @@ var __extends = (this && this.__extends) || function (d, b) {
 /// <reference path="../definitelyTyped/preloadjs.d.ts" />
 var Ticker = createjs.Ticker;
 var FIELD_SIZE = 40;
+var STEP_SIZE = 1;
 function init() {
     document.onkeydown = keyPressed;
     var stage = new createjs.Stage("demoCanvas");
     createjs.Ticker.addEventListener("tick", handleTick);
+    createjs.Ticker.paused = !createjs.Ticker.paused;
     var gamefield = new Field(3, 3);
     var source = new Source(0, 2, Direction.East);
     gamefield.field[0][2] = source;
     gamefield.field[2][2] = new Mirror(2, 2, Alignment.BOTTOM_LEFT_TO_TOP_RIGHT);
-    gamefield.field[2][0] = new Detector(2, 0, Direction.South);
-    var laser = new Laser(source.xPos, source.yPos, source.direction);
+    gamefield.field[2][0] = new Detector(2, 0, Direction.North);
+    gamefield.field[2][1] = new Block(2, 1, BlockAlignment.HORIZONTAL);
+    var laser = new Laser(source.xPos, source.yPos, source.direction, gamefield);
+    gamefield.render(stage);
+    stage.update();
     function handleTick(event) {
         if (!createjs.Ticker.paused) {
-            gamefield.render(stage);
+            laser.move();
             laser.render(stage);
             stage.update();
+            if (laser.won) {
+                createjs.Ticker.paused = true;
+                console.log("won");
+            }
         }
     }
 }
@@ -30,6 +39,10 @@ function keyPressed(event) {
     console.log(event.keyCode);
     switch (event.keyCode) {
         case 80:
+            createjs.Ticker.paused = !createjs.Ticker.paused;
+            console.log("pause");
+            break;
+        case 83:
             createjs.Ticker.paused = !createjs.Ticker.paused;
             console.log("pause");
             break;
