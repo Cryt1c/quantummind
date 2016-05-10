e3473c55b0d75dc94e85e15932fb94264679d2b7
Cryt1c <david@peherstorfer.info>
2016-05-10 10:25:30 +0200

    setup typescript createjs project

---
+++ quantummind/js/core.js
@@ -0,0 +1,14 @@
+/// <reference path="../definitelyTyped/easeljs.d.ts" />
+/// <reference path="../definitelyTyped/jquery.d.ts" />
+/// <reference path="../definitelyTyped/preloadjs.d.ts" />
+function init() {
+    var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.addChild(new createjs.Shape()).set({ x: 100, y: 100 }).graphics.f("red").dc(0, 0, 50);
+    stage.update();
+}
+//# sourceMappingURL=core.js.map
