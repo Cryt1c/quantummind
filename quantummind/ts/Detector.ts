653cac55305c4b39186b066851e59e56dc151d6e
Dominik Seitz <seitzdo@gmail.com>
2016-05-10 11:16:50 +0200

    added class Detector

---
+++ quantummind/ts/Detector.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Dominik on 10.05.2016.
+ */
+
+class Detector extends GameElement {
+    constructor(xPos:number, yPos:number, public direction: Direction) {
+        super(xPos, yPos, 1, 1);
+    }
+}
