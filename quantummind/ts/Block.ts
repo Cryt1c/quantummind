d14d378e228346419a313ed11ede5b633c51a1a5
Dominik Seitz <seitzdo@gmail.com>
2016-05-17 12:43:04 +0200

    Merge branch 'master' of https://github.com/Cryt1c/quantummind
    Conflicts:
    	quantummind/ts/Field.ts

--- quantummind/ts/Block.ts
+++ quantummind/ts/Block.ts
@@ -2,7 +2,7 @@ class Block extends GameElement{
     constructor(stage:createjs.Stage, xPos:number, yPos:number, public alignment:BlockAlignment) {
         super(stage, xPos, yPos, 1, 1);
         // TODO use different bitmaps for different alignments
-        super.initBitmap("block.png");
+        super.initBitmap("block");
     }
 }
 
